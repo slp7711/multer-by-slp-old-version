956657d6ed1cd3982594eaeb263693e20ba8cf86
Sergio Luiz Paganini <slp7711@gmail.com>
2021-05-08 21:21:27 -0300

    first commit on this project

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const PORT = process.env.port || 3000
+
+
+const app = express()
+
+
+app.use('/api', require('./routes/api'))
+
+
+app.listen(PORT, () => {
+    console.log('Server runniong on port ' + PORT);
+});
+
---
+++ server/models/imoveis.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const ImoveisSchema = new mongoose.Schema({
+    type: {
+        type: String,
+        enum: ['casa', 'apartamento', 'sala comercial', 'imovel rural']
+    },
+    description: {type: String},
+    price: {type: String},
+    imageName: [String]
+})
+
+const Imovel = mongoose.model('imoveis', ImoveisSchema)
+
+
+module.exports = Imovel;
